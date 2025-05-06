cad5a0f096d7ce5d79a919a6068f36e21f5ba2c3
Sloeffler123 <samloeffler123@gmail.com>
2025-05-06 08:00:07 -0500

    space race sunburst complete

--- sunburst-all-data.py
+++ sunburst-all-data.py
@@ -1,8 +1,5 @@
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-
 
 data = pd.read_csv('rocket-data.csv')
 
@@ -10,7 +7,6 @@ companys = data['company'].values,
 count = data['company'].value_counts().reset_index(),
 outcomes = data['outcome'].value_counts().reset_index(),
                      
-
 fig = px.sunburst(data, path=['company', 'outcome'])
 
 fig.show()
--- us-russia-space-race-sunburst.py
+++ us-russia-space-race-sunburst.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import plotly.express as px
 
-us_data = all_data[all_data['company'] == 'NASA']
-russia_data = all_data[all_data['company'] == 'RVSN USSR']
+us_data = pd.read_csv('us-space-race-data.csv')
+russia_data = pd.read_csv('russia-space-race-data.csv')
 
 combined = pd.concat([us_data, russia_data])
 
-
-fig = px.sunburst(combined, path=['company','outcome', 'date'])
+fig = px.sunburst(combined, path=['company','outcome', 'date', 'location'])
 
 fig.show()
